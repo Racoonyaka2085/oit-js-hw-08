31ec43cbfd578409f7f584a9e1c709d554a982a7
Vitalii <vitalya.boss82@gmail.com>
2022-04-12 19:49:17 +0300

    Update 03-feedback.js
    Added finally fixes(mabe)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,29 +4,35 @@ const formRef = document.querySelector('.feedback-form');
 
 const formData = {};
 
-formRef.addEventListener('input', throttle((event) => {
-    event.preventDefault();
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}, 500));
+dataFromLocalStorage();
 
-formRef.addEventListener('submit', (event) => {
+formRef.addEventListener(
+    'input',
+    throttle(event => {
+        event.preventDefault();
+        formData[event.target.name] = event.target.value;
+        localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    }, 500),
+);
+
+formRef.addEventListener('submit', event => {
     event.preventDefault();
-    if (event.target.email.value === "" || event.target.message.value === "") {
-        return;
+    if (event.target.email.value === '' || event.target.message.value === '') {
+        return alert('Please fill in all the fields!');
     }
+
     console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
     event.currentTarget.reset();
     localStorage.removeItem('feedback-form-state');
-})
+});
 
 function dataFromLocalStorage() {
-    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-    const email = document.querySelector('.feedback-form input');
-    const message = document.querySelector('.feedback-form textarea');
+    let data = localStorage.getItem('feedback-form-state');
     if (data) {
-        email.value = data.email;
-        message.value = data.message;
+        data = JSON.parse(data);
+        Object.entries(data).forEach(([name, value]) => {
+            formData[name] = value;
+            formRef.elements[name].value = value;
+        });
     }
 }
-dataFromLocalStorage();
